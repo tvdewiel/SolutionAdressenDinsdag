039bdc6258bb8a596dc203245b2e1c41553a94cb
Tom Vande Wiele <tvdewiel@hotmail.com>
2024-03-26 09:41:16 +0100

    Add project files.

---
+++ AdressenBL/Model/Gemeente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdressenBL.Model
+{
+    public class Gemeente
+    {
+        public int Id { get; set; }
+        public string Naam { get; set; }
+        private SortedSet<string> straatNamen =new SortedSet<string>();
+        public IReadOnlyList<string> GeefStraatNamen()=> straatNamen.ToList();
+        public void VoegStraatnaamToe(string straatnaam)
+        {
+            straatNamen.Add(straatnaam);
+        }
+    }
+}
---
+++ AdressenBL/Model/Provincie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdressenBL.Model
+{
+    public class Provincie
+    {
+        public int Id { get; set; }
+        public string Naam { get; set; }
+        private Dictionary<int,Gemeente> gemeentes =new Dictionary<int,Gemeente>();
+        public IReadOnlyList<Gemeente> GeefGemeentes()=>gemeentes.Values.ToList();
+        public void VoegGemeenteToe(Gemeente gemeente)
+        {
+            gemeentes.TryAdd(gemeente.Id, gemeente);
+        }
+    }
+}
